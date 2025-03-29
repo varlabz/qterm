b6f4de0ca784ac403a7bd335d905f0fb12f1b088
var <github@umdoze.com>
2025-03-29 13:17:45 -0700

    add mcp dependencies

--- src/mcp.ts
+++ src/mcp.ts
@@ -1,16 +1,15 @@
-// import { MultiServerMCPClient } from '@langchain/mcp-adapters';
+import { MultiServerMCPClient } from '@langchain/mcp-adapters';
 import { StructuredToolInterface } from '@langchain/core/tools';
 
 export async function initializeTools(file: string): Promise<StructuredToolInterface[]> {
-    return [];
-    // try {
-    //     const client = await MultiServerMCPClient.fromConfigFile(file);
-    //     await client.initializeConnections();
-    //     const tools = await client.getTools();
-    //     // console.log(`Initialized ${tools.length} MCP tools from ${file}`);
-    //     return tools;
-    // } catch (error) {
-    //     console.error(`Failed to initialize MCP tools: ${error instanceof Error ? error.message : String(error)}`);
-    //     return [];
-    // }
+    try {
+        const client = await MultiServerMCPClient.fromConfigFile(file);
+        await client.initializeConnections();
+        const tools = await client.getTools();
+        // console.log(`Initialized ${tools.length} MCP tools from ${file}`);
+        return tools;
+    } catch (error) {
+        console.error(`Failed to initialize MCP tools: ${error instanceof Error ? error.message : String(error)}`);
+        return [];
+    }
 }
